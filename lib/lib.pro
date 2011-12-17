4d0208d2530f6f837c940a262ac9765c9f523f52
Adriano Rezende <adriano.rezende@openbossa.org>
2011-12-17 20:10:22 -0300

    Added Sprite, SpriteState and SpriteScene components
    These components can be used to create sprite elements for
    QML based games. They manage frames and states of sprites
    and they can be easily integrated with other physics engine
    like QmlBox2D.
    
    Signed-off-by: Adriano Rezende <adriano.rezende@openbossa.org>

---
+++ lib/lib.pro
@@ -0,0 +1,22 @@
+TEMPLATE = lib
+CONFIG += plugin
+TARGET = expresso
+
+QT += declarative
+
+HEADERS += plugin.h \
+           sprite.h
+
+SOURCES += plugin.cpp \
+           sprite.cpp
+
+QML_SOURCES += qmldir
+
+INSTALL_PATH = $$[QT_INSTALL_IMPORTS]/Expresso
+
+target.path = $$INSTALL_PATH
+
+qmlfiles.files = $$QML_SOURCES
+qmlfiles.path = $$INSTALL_PATH
+
+INSTALLS += target qmlfiles
---
+++ lib/plugin.cpp
@@ -0,0 +1,32 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Institute of Technology.
+** All rights reserved.
+** Contact: Manager (renato.chencarek@openbossa.org)
+**
+** This file is part of the Expresso project.
+**
+** GNU Lesser General Public License Usage
+**
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+****************************************************************************/
+
+#include "plugin.h"
+#include "sprite.h"
+
+
+void ExpressoPlugin::registerTypes(const char *uri)
+{
+    Q_ASSERT(uri == QLatin1String("Expresso"));
+    qmlRegisterType<Sprite>(uri, 1, 0, "Sprite");
+    qmlRegisterType<SpriteState>(uri, 1, 0, "SpriteState");
+    qmlRegisterType<SpriteScene>(uri, 1, 0, "SpriteScene");
+}
+
+Q_EXPORT_PLUGIN2(expressoplugin, ExpressoPlugin);
